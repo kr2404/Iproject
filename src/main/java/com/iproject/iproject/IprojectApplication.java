da09fdb4e97b9daa282a6ba57087243e1d6fdb50
KarimR <karim.rebah@gmail.com>
2018-10-31 00:05:35 +0100

    First step

---
+++ src/main/java/com/iproject/iproject/IprojectApplication.java
@@ -0,0 +1,12 @@
+package com.iproject.iproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprojectApplication.class, args);
+	}
+}
