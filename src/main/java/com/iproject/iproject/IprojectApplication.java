e2220c797b0d47b3e303b79c0d24b4fd079ab794
KarimR <karim.rebah@gmail.com>
2018-10-31 21:25:10 +0100

    Master : test

--- src/main/java/com/iproject/iproject/IprojectApplication.java
+++ src/main/java/com/iproject/iproject/IprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IprojectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(IprojectApplication.class, args);
 	}
 }
